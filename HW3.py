14114e14053361588c9e6ac12664e1986e5dca19
Lilia <lilia.samosonok@gmail.com>
2024-02-04 01:21:54 +0000

    added modified code

--- HW3.py
+++ HW3.py
@@ -4,25 +4,28 @@
 # The program, based on the operation, calculates and prints the result.
 # Include a check to ensure that the divisor is not equal to 0 when performing division!
 
-input_first_number = float(input("Please enter 1st number: "))
-input_second_number = float(input("Please enter 2nd number: "))
+while True:
+    input_first_number = float(input("Please enter 1st number: "))
+    input_second_number = float(input("Please enter 2nd number: "))
+    chosen_operator = input("Please choose operator: '+' '-' '*' '/' ")
 
-chosen_operator = input("Please chose operator: "
-                        "\nEnter 1 - for '+'"
-                        "\nEnter 2 - for '-'"
-                        "\nEnter 3 - for '*'"
-                        "\nEnter 4 - for '/'")
-
-if chosen_operator == "1":
-    print(input_first_number + input_second_number)
-elif chosen_operator == "2":
-    print(input_first_number - input_second_number)
-elif chosen_operator == "3":
-    print(input_first_number * input_second_number)
-elif chosen_operator == "4":
-    if input_second_number == 0:
-        print("Error! Division by 0")
+    result = 0
+    if chosen_operator == "+":
+        result = input_first_number + input_second_number
+    elif chosen_operator == "-":
+        result = input_first_number - input_second_number
+    elif chosen_operator == "*":
+        result = input_first_number * input_second_number
+    elif chosen_operator == "/":
+        if input_second_number == 0:
+            print("Error! Division by 0")
+        else:
+            result = input_first_number / input_second_number
     else:
-        print(input_first_number / input_second_number)
-else:
-    print("You have to choose one of the numbers!")
+        print("You have to choose one of the options!")
+        break
+    print("Result: ", result)
+
+    another_calculation = input("Would you like to do another calculation? 1 - yes, 2 - no: ")
+    if another_calculation != "1":
+        break
